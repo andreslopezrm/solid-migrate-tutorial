8a9041c1b95a3768876ef8a85668cd4b46c891d0
Andres <andres@dreamsengine.com>
2024-11-10 10:15:57 -0600

    ✨ Base config

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Title } from "@solidjs/meta";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Hello World</Title>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  );
+}
