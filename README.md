ec841c495fa7e90389b75413c7f94212e5823a16
Andres <andres@dreamsengine.com>
2024-11-12 23:21:45 -0600

    ✨ Readme update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# solid-migrate-tutorial
+# Solid migration tutorial
+
+![solid](/public/images/cover.png)
+
+Guide showing some key features of Solid applied to other libraries
+
+- Solid
+- React
+- Angular
+- Vue
+- Solid
+- Lit
+- Qwik
+
+[https://solid-migration.netlify.app/](https://solid-migration.netlify.app/)
+
+Tutorial for Solid Hackathon 2024
--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -8,7 +8,15 @@ export function MainTitle({ text }: Props) {
 	return (
 		<div class="flex">
 			<Drawer />
-			<h1 class="text-3xl md:text-4xl font-bold text-gray-900">{text}</h1>
+			<div class="flex-1 flex justify-between items-center gap-2">
+				<h1 class="text-3xl md:text-4xl font-bold text-gray-900">{text}</h1>
+				<a
+					href="https://github.com/andreslopezrm/solid-migrate-tutorial"
+					target="_blank"
+				>
+					<img src="/images/github.png" width={38} />
+				</a>
+			</div>
 		</div>
 	);
 }
